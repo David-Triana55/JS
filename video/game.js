0194030b823b1a92ffcd3cc60646f9c6405017ff
David Triana <davidtriana796@gmail.com>
2023-09-11 20:24:33 -0500

    inicio juego

---
+++ video/game.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame() {
+    // game.fillReact(0,0,100,50)
+    // game.clearReact(0,0,100,50)
+    // game.filltext("hola",0,100,)
+    // game.fillStyle = "black"
+    // game.font = "Helvetica"
+    // game.textAlign = "left"
+
+    
+}
