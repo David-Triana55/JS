6a70953ff0173d7aa6b6c072d2edbcc41504e433
David Triana <davidtriana796@gmail.com>
2023-10-19 19:45:27 -0500

    clean code

--- POO/static.js
+++ POO/static.js
@@ -483,3 +483,21 @@ SuperObject.deepCopy = function (subject) {
     }
     return copySubject
 }
+
+
+// Antes de ES6
+function Person(name) {
+    this.name = name;
+  }
+  var person = new Person("Alex");
+  console.log(person.name); //> Alex
+  
+  // Con ES6
+  class Person {
+    constructor(person) {
+      this.name = name;
+    }
+  }
+  
+  var person = new Person("Alex");
+  console.log(person.name); //> Alex
